abea8acf6981c3697b9bdfb9de4d26aaeab49a26
ladyada <limor@ladyada.net>
2020-05-16 11:31:56 -0400

    clang!

--- Adafruit_SSD1305.cpp
+++ Adafruit_SSD1305.cpp
@@ -12,16 +12,17 @@ products from Adafruit!
 
 Written by Limor Fried/Ladyada  for Adafruit Industries.
 BSD license, check license.txt for more information
-All text above, and the splash screen below must be included in any redistribution
+All text above, and the splash screen below must be included in any
+redistribution
 *********************************************************************/
 
 #ifdef __AVR__
- #include <avr/pgmspace.h>
- #include <util/delay.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
 #elif defined(ESP8266)
- #include <pgmspace.h>
+#include <pgmspace.h>
 #else
- #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+#define pgm_read_byte(addr) (*(const unsigned char *)(addr))
 #endif
 
 #include "Adafruit_SSD1305.h"
@@ -63,10 +64,9 @@ All text above, and the splash screen below must be included in any redistributi
             allocation is performed there!
 */
 Adafruit_SSD1305::Adafruit_SSD1305(uint16_t w, uint16_t h, TwoWire *twi,
-				 int8_t rst_pin, uint32_t clkDuring, uint32_t clkAfter) :
-  Adafruit_MonoOLED(w, h, twi, rst_pin, clkDuring, clkAfter)
-{
-}
+                                   int8_t rst_pin, uint32_t clkDuring,
+                                   uint32_t clkAfter)
+    : Adafruit_MonoOLED(w, h, twi, rst_pin, clkDuring, clkAfter) {}
 
 /*!
     @brief  Constructor for SPI SSD1305 displays, using software (bitbang)
@@ -95,9 +95,10 @@ Adafruit_SSD1305::Adafruit_SSD1305(uint16_t w, uint16_t h, TwoWire *twi,
     @note   Call the object's begin() function before use -- buffer
             allocation is performed there!
 */
-Adafruit_SSD1305::Adafruit_SSD1305(uint16_t w, uint16_t h, int8_t mosi_pin, 
-				   int8_t sclk_pin, int8_t dc_pin, int8_t rst_pin, int8_t cs_pin) : Adafruit_MonoOLED(w, h, mosi_pin, sclk_pin, dc_pin, rst_pin, cs_pin) {
-}
+Adafruit_SSD1305::Adafruit_SSD1305(uint16_t w, uint16_t h, int8_t mosi_pin,
+                                   int8_t sclk_pin, int8_t dc_pin,
+                                   int8_t rst_pin, int8_t cs_pin)
+    : Adafruit_MonoOLED(w, h, mosi_pin, sclk_pin, dc_pin, rst_pin, cs_pin) {}
 
 /*!
     @brief  Constructor for SPI SSD1305 displays, using native hardware SPI.
@@ -126,16 +127,14 @@ Adafruit_SSD1305::Adafruit_SSD1305(uint16_t w, uint16_t h, int8_t mosi_pin,
             allocation is performed there!
 */
 Adafruit_SSD1305::Adafruit_SSD1305(uint16_t w, uint16_t h, SPIClass *spi,
-    int8_t dc_pin, int8_t rst_pin, int8_t cs_pin, uint32_t bitrate) :
-  Adafruit_MonoOLED(w, h, spi, dc_pin, rst_pin, cs_pin, bitrate) {
-}
+                                   int8_t dc_pin, int8_t rst_pin, int8_t cs_pin,
+                                   uint32_t bitrate)
+    : Adafruit_MonoOLED(w, h, spi, dc_pin, rst_pin, cs_pin, bitrate) {}
 
 /*!
     @brief  Destructor for Adafruit_SSD1305 object.
 */
-Adafruit_SSD1305::~Adafruit_SSD1305(void) {
-}
-
+Adafruit_SSD1305::~Adafruit_SSD1305(void) {}
 
 // ALLOCATE & INIT DISPLAY -------------------------------------------------
 
@@ -161,79 +160,93 @@ Adafruit_SSD1305::~Adafruit_SSD1305(void) {
 */
 bool Adafruit_SSD1305::begin(uint8_t addr, bool reset) {
 
-  if (! Adafruit_MonoOLED::_init(addr, reset)) {
+  if (!Adafruit_MonoOLED::_init(addr, reset)) {
     return false;
   }
 
-  drawBitmap((WIDTH - splash2_width) / 2, 
-	     (HEIGHT - splash2_height) / 2,
-	     splash2_data, splash2_width, splash2_height, 1);
+  drawBitmap((WIDTH - splash2_width) / 2, (HEIGHT - splash2_height) / 2,
+             splash2_data, splash2_width, splash2_height, 1);
 
   // Init sequence, make sure its under 32 bytes, or split into multiples!
   static const uint8_t init_128x32[] = {
-    // Init sequence for 128x32 OLED module
-    SSD1305_DISPLAYOFF,                  // 0xAE
-    SSD1305_SETLOWCOLUMN | 0x0,          // low col = 0
-    SSD1305_SETHIGHCOLUMN | 0x0,         // hi col = 0
-    SSD1305_SETSTARTLINE | 0x0,          // line #0
-    0x2E,                                // ??
-    SSD1305_SETCONTRAST, 0x32,           // 0x81, 0x32
-    SSD1305_SETBRIGHTNESS, 0x10,         // 0x82, 0x10
-    SSD1305_SEGREMAP | 0x00,
-    SSD1305_NORMALDISPLAY,               // 0xA6
-    SSD1305_SETMULTIPLEX, 0x3F,          // 0xA8, 0x3F (1/64)
-    SSD1305_MASTERCONFIG, 0x8E,          // external vcc supply
-    SSD1305_COMSCANDEC,
-    SSD1305_SETDISPLAYOFFSET, 0x40,      // 0xD3, 0x40
-    SSD1305_SETDISPLAYCLOCKDIV, 0xF0,    // 0xD5, 0xF0
-    SSD1305_SETAREACOLOR, 0x05,
-    SSD1305_SETPRECHARGE, 0xF1,          // 0xd9, 0xF1
-    SSD1305_SETCOMPINS, 0x12,            // 0xDA, 0x12
-    SSD1305_SETLUT,
-    0x3F,
-    0x3F,
-    0x3F,
-    0x3F
-  };
+      // Init sequence for 128x32 OLED module
+      SSD1305_DISPLAYOFF,          // 0xAE
+      SSD1305_SETLOWCOLUMN | 0x0,  // low col = 0
+      SSD1305_SETHIGHCOLUMN | 0x0, // hi col = 0
+      SSD1305_SETSTARTLINE | 0x0,  // line #0
+      0x2E,                        // ??
+      SSD1305_SETCONTRAST,
+      0x32, // 0x81, 0x32
+      SSD1305_SETBRIGHTNESS,
+      0x10, // 0x82, 0x10
+      SSD1305_SEGREMAP | 0x00,
+      SSD1305_NORMALDISPLAY, // 0xA6
+      SSD1305_SETMULTIPLEX,
+      0x3F, // 0xA8, 0x3F (1/64)
+      SSD1305_MASTERCONFIG,
+      0x8E, // external vcc supply
+      SSD1305_COMSCANDEC,
+      SSD1305_SETDISPLAYOFFSET,
+      0x40, // 0xD3, 0x40
+      SSD1305_SETDISPLAYCLOCKDIV,
+      0xF0, // 0xD5, 0xF0
+      SSD1305_SETAREACOLOR,
+      0x05,
+      SSD1305_SETPRECHARGE,
+      0xF1, // 0xd9, 0xF1
+      SSD1305_SETCOMPINS,
+      0x12, // 0xDA, 0x12
+      SSD1305_SETLUT,
+      0x3F,
+      0x3F,
+      0x3F,
+      0x3F};
   static const uint8_t init_128x64[] = {
-    // Init sequence for 128x32 OLED module
-    SSD1305_DISPLAYOFF,                  // 0xAE
-    SSD1305_SETLOWCOLUMN | 0x4,            // low col = 0
-    SSD1305_SETHIGHCOLUMN | 0x4,           // hi col = 0
-    SSD1305_SETSTARTLINE | 0x0,            // line #0
-    0x2E,                          //??
-    SSD1305_SETCONTRAST, 0x32,                 // 0x81, 0x32
-    SSD1305_SETBRIGHTNESS, 0x80,                 // 0x82, 0x80
-    SSD1305_SEGREMAP | 0x01,
-    SSD1305_NORMALDISPLAY,                 // 0xA6
-    SSD1305_SETMULTIPLEX, 0x3F,                  // 0xA8, 0x3F (1/64)
-    SSD1305_MASTERCONFIG, 0x8E,                 /* external vcc supply */
-    SSD1305_COMSCANDEC,
-    SSD1305_SETDISPLAYOFFSET, 0x40,              // 0xD3, 0x40
-    SSD1305_SETDISPLAYCLOCKDIV, 0xF0,           // 0xD5, 0xF0
-    SSD1305_SETAREACOLOR, 0x05,
-    SSD1305_SETPRECHARGE, 0xF1,                // 0xd9, 0xF1
-    SSD1305_SETCOMPINS, 0x12,                    // 0xDA, 0x12
-    SSD1305_SETLUT,
-    0x3F,
-    0x3F,
-    0x3F,
-    0x3F
-  };
+      // Init sequence for 128x32 OLED module
+      SSD1305_DISPLAYOFF,          // 0xAE
+      SSD1305_SETLOWCOLUMN | 0x4,  // low col = 0
+      SSD1305_SETHIGHCOLUMN | 0x4, // hi col = 0
+      SSD1305_SETSTARTLINE | 0x0,  // line #0
+      0x2E,                        //??
+      SSD1305_SETCONTRAST,
+      0x32, // 0x81, 0x32
+      SSD1305_SETBRIGHTNESS,
+      0x80, // 0x82, 0x80
+      SSD1305_SEGREMAP | 0x01,
+      SSD1305_NORMALDISPLAY, // 0xA6
+      SSD1305_SETMULTIPLEX,
+      0x3F, // 0xA8, 0x3F (1/64)
+      SSD1305_MASTERCONFIG,
+      0x8E, /* external vcc supply */
+      SSD1305_COMSCANDEC,
+      SSD1305_SETDISPLAYOFFSET,
+      0x40, // 0xD3, 0x40
+      SSD1305_SETDISPLAYCLOCKDIV,
+      0xF0, // 0xD5, 0xF0
+      SSD1305_SETAREACOLOR,
+      0x05,
+      SSD1305_SETPRECHARGE,
+      0xF1, // 0xd9, 0xF1
+      SSD1305_SETCOMPINS,
+      0x12, // 0xDA, 0x12
+      SSD1305_SETLUT,
+      0x3F,
+      0x3F,
+      0x3F,
+      0x3F};
 
-
-  if ((HEIGHT == 64) && (! oled_commandList(init_128x64, sizeof(init_128x64)))) {
+  if ((HEIGHT == 64) && (!oled_commandList(init_128x64, sizeof(init_128x64)))) {
     return false;
-  } else if (HEIGHT == 32 && (! oled_commandList(init_128x32, sizeof(init_128x32)))) {
+  } else if (HEIGHT == 32 &&
+             (!oled_commandList(init_128x32, sizeof(init_128x32)))) {
     return false;
   }
-  delay(100); // 100ms delay recommended
-  oled_command(SSD1305_DISPLAYON);                   // 0xaf
+  delay(100);                      // 100ms delay recommended
+  oled_command(SSD1305_DISPLAYON); // 0xaf
   setContrast(0x2F);
 
   return true; // Success
 }
-
 
 void Adafruit_SSD1305::display(void) {
   // ESP8266 needs a periodic yield() call to avoid watchdog reset.
@@ -245,7 +258,7 @@ void Adafruit_SSD1305::display(void) {
   yield();
 
   uint16_t count = WIDTH * ((HEIGHT + 7) / 8);
-  uint8_t *ptr   = buffer;
+  uint8_t *ptr = buffer;
   uint8_t dc_byte = 0x40;
   uint8_t pages = ((HEIGHT + 7) / 8);
 
@@ -265,17 +278,17 @@ void Adafruit_SSD1305::display(void) {
   */
 
   uint8_t first_page = window_y1 / 8;
-  uint8_t last_page = (window_y2+7) / 8;
+  uint8_t last_page = (window_y2 + 7) / 8;
   uint8_t page_start = min((int16_t)bytes_per_page, window_x1);
   uint8_t page_end = max((int16_t)0, window_x2);
   /*
-  Serial.print("Pages: ");  
+  Serial.print("Pages: ");
   Serial.print(first_page);
   Serial.print(" -> ");
   Serial.println(last_page);
   pages = min(pages, last_page);
 
-  Serial.print("Page addr: ");  
+  Serial.print("Page addr: ");
   Serial.print(page_start);
   Serial.print(" -> ");
   Serial.println(page_end);
@@ -292,26 +305,26 @@ void Adafruit_SSD1305::display(void) {
     maxbuff = i2c_dev->maxBufferSize() - 1;
   }
 
-  for (uint8_t p=first_page; p<pages; p++) {
+  for (uint8_t p = first_page; p < pages; p++) {
     uint8_t bytes_remaining = bytes_per_page;
     ptr = buffer + (uint16_t)p * (uint16_t)bytes_per_page;
     // fast forward to dirty rectangle beginning
     ptr += page_start;
     bytes_remaining -= page_start;
     // cut off end of dirty rectangle
-    bytes_remaining -= (WIDTH-1) - page_end;
-    
-    uint8_t cmd[] = {SSD1305_SETPAGESTART + p, 
-		     0x10 + (page_start >> 4), page_start & 0xF};
+    bytes_remaining -= (WIDTH - 1) - page_end;
+
+    uint8_t cmd[] = {SSD1305_SETPAGESTART + p, 0x10 + (page_start >> 4),
+                     page_start & 0xF};
     oled_commandList(cmd, sizeof(cmd));
-    
+
     while (bytes_remaining) {
       uint8_t to_write = min(bytes_remaining, maxbuff);
       if (i2c_dev) {
-	i2c_dev->write(ptr, to_write, true, &dc_byte, 1);
+        i2c_dev->write(ptr, to_write, true, &dc_byte, 1);
       } else {
-	digitalWrite(dcPin, HIGH);
-	spi_dev->write(ptr, to_write);
+        digitalWrite(dcPin, HIGH);
+        spi_dev->write(ptr, to_write);
       }
       ptr += to_write;
       bytes_remaining -= to_write;
@@ -330,4 +343,3 @@ void Adafruit_SSD1305::display(void) {
   window_x2 = -1;
   window_y2 = -1;
 }
-
--- Adafruit_SSD1305.h
+++ Adafruit_SSD1305.h
@@ -15,14 +15,12 @@ BSD license, check license.txt for more information
 All text above, and the splash screen must be included in any redistribution
 *********************************************************************/
 
-
 #include <Adafruit_MonoOLED.h>
-
 
 #define BLACK 0
 #define WHITE 1
 
-#define SSD1305_I2C_ADDRESS   0x3C	// 011110+SA0+RW - 0x3C or 0x3D
+#define SSD1305_I2C_ADDRESS 0x3C // 011110+SA0+RW - 0x3C or 0x3D
 
 #define SSD1305_SETLOWCOLUMN 0x00
 #define SSD1305_SETHIGHCOLUMN 0x10
@@ -58,18 +56,18 @@ All text above, and the splash screen must be included in any redistribution
 #define SSD1305_SETCOMPINS 0xDA
 #define SSD1305_SETVCOMLEVEL 0xDB
 
-
 class Adafruit_SSD1305 : public Adafruit_MonoOLED {
- public:
-  Adafruit_SSD1305(uint16_t w, uint16_t h, TwoWire *twi=&Wire, int8_t rst_pin=-1,
-		    uint32_t preclk=400000, uint32_t postclk=100000);
+public:
+  Adafruit_SSD1305(uint16_t w, uint16_t h, TwoWire *twi = &Wire,
+                   int8_t rst_pin = -1, uint32_t preclk = 400000,
+                   uint32_t postclk = 100000);
   Adafruit_SSD1305(uint16_t w, uint16_t h, int8_t mosi_pin, int8_t sclk_pin,
-    int8_t dc_pin, int8_t rst_pin, int8_t cs_pin);
-  Adafruit_SSD1305(uint16_t w, uint16_t h, SPIClass *spi,
-    int8_t dc_pin, int8_t rst_pin, int8_t cs_pin, uint32_t bitrate=8000000UL);
+                   int8_t dc_pin, int8_t rst_pin, int8_t cs_pin);
+  Adafruit_SSD1305(uint16_t w, uint16_t h, SPIClass *spi, int8_t dc_pin,
+                   int8_t rst_pin, int8_t cs_pin, uint32_t bitrate = 8000000UL);
   ~Adafruit_SSD1305(void);
 
-  bool begin(uint8_t i2caddr = SSD1305_I2C_ADDRESS, bool reset=true);
+  bool begin(uint8_t i2caddr = SSD1305_I2C_ADDRESS, bool reset = true);
   void display();
 
 private:
